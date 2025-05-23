d4f630cd6fb659f43df85bd993e6a52771eba2e6
yujiajia <yujiajia@asteria.com>
2025-05-23 10:33:55 +0800

    init
    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Crispy
+
+First, run the development server:
+
+```bash
+bun dev
+```
+
+Open [http://localhost:4000](http://localhost:4000)
+
+## Next.js Resources
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+## Deploy on self-host
+
+```bash
+bun build
+bun start
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
+  {
+    ignores: ['node_modules', '.next'],
+  },
+  js.configs.recommended,
+  ...compat.extends(
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'next',
+    'next/core-web-vitals',
+    'next/typescript'
+  ),
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: false,
+          trailingComma: 'es5',
+          tabWidth: 2,
+          printWidth: 100,
+          bracketSpacing: true,
+          arrowParens: 'avoid',
+          endOfLine: 'lf',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['hrefLeft', 'hrefRight'],
+          aspects: ['invalidHref', 'preferButton'],
+        },
+      ],
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+    },
+  },
+]
+
+export default eslintConfig
