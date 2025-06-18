e62db23950cac2f18feed4f9f2a92d39723e1e6e
yujiajia <yujiajia@asteria.com>
2025-06-18 18:23:11 +0800

    cros token

--- src/server/middleware/cors.ts
+++ src/server/middleware/cors.ts
@@ -6,7 +6,7 @@ export const corsMiddleware: RequestHandler = (req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, App_name, Channel, Cache-control, X-access-token'
   )
 
   if (req.method === 'OPTIONS') {
